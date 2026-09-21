7416ec0dc0524da45cd9e7abd70213ad6ab3d89e
Philip Loche <ploche@physik.fu-berlin.de>
2026-09-21 23:02:27 +0200

    chore: version comes only from setuptools_scm

--- src/cra/__init__.py
+++ src/cra/__init__.py
@@ -1,10 +1,5 @@
 """Cluster Research Assistant."""
 
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("cluster-research-assist")
-except PackageNotFoundError:  # running from a checkout without an install
-    __version__ = "0.0.0"
+from cra._version import __version__
 
 __all__ = ["__version__"]
